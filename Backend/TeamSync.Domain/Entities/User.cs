0270a4e8c0d426877cd105894770defb60cdfb62
Rejwanul Haque <rejwanul7296@gmail.com>
2025-11-19 20:08:13 +0600

    feat: implement BaseEntity for common properties and refactor Project, TaskItem, and User entities to inherit from it

--- Backend/TeamSync.Domain/Entities/User.cs
+++ Backend/TeamSync.Domain/Entities/User.cs
@@ -3,11 +3,8 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace TeamSync.Domain.Entities
 {
-	public class User
+	public class User : BaseEntity
 	{
-		[BsonId] // tells Mongo this is the document's primary key
-		[BsonRepresentation(BsonType.ObjectId)]
-		public string Id { get; set; } = string.Empty;
 		public string Name { get; set; } = "Unknown";
 		public string Email { get; set; } = string.Empty;
 		public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
